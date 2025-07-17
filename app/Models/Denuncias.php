02a91b7b02bf40be9ee22d21a1732f8d0285b9fb
Kivan <chaquira73@gmail.com>
2025-07-17 16:01:48 -0400

    Nuevas

--- app/Models/Denuncias.php
+++ app/Models/Denuncias.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Denuncias extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'motivo',
+        'reportado_id',
+        'reportante_id',
+    ];
+
 }
--- app/Models/Visitas.php
+++ app/Models/Visitas.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Visitas extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'fecha_hora',
+        'estado',
+        'propiedad_id',
+        'usuario_id'
+    ];
 }
