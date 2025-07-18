8ef4adad324fef7d164254eab89db911fbdf48c2
SergioWalker11 <romero75122443gmail.com>
2025-07-18 08:34:45 -0400

    primer controller

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+class UsuarioController extends Controller
+{
+    public function Index(){
+        $usuarios = Usuario::all();
+        return view('usuarios.index', compact('usuarios'));
+    }
+}
