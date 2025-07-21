8fffa97aafd6c5e8e6719ac3be9653c4f177d226
Kivan <chaquira73@gmail.com>
2025-07-21 08:29:09 -0400

    mini cambios

--- app/Http/Controllers/PropiedadController.php
+++ app/Http/Controllers/PropiedadController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Propiedad;
+use App\Models\ImagenPropiedad;
 
 class PropiedadController extends Controller
 {
     public function ListarPropiedad()
     {
         $propiedades = Propiedad::all();
-        return view('usuario.listarpropiedad', compact('propiedades'));
+        return view('propietario.listarpropiedad', compact('propiedades'));
     }
      public function CrearPropiedad(){
 
-        return view('usuario.crearpropiedad');
+        return view('usuario.propietario.listarpropiedad');
 
     }
+
 
 }
