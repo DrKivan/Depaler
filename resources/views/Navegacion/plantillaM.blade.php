c5b0e62aec909f56904068a24ed5451ed49d3118
Kivan <chaquira73@gmail.com>
2025-07-23 16:58:04 -0400

    SE PUEDE DESBANEAR

--- resources/views/Navegacion/plantillaM.blade.php
+++ resources/views/Navegacion/plantillaM.blade.php
@@ -6,7 +6,12 @@
   <title>Navbar Responsive</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
+<!-- CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+<!-- Default theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 </head>
 <body class="bg-gray-100">
 
